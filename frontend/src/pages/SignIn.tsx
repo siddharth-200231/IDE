e20dc0e8b808c1fe589c236a0f787102461635b5
Siddharth <sahusidd715@gmail.com>
2025-02-17 10:34:17 +0530

    modified:   frontend/src/pages/SignIn.tsx 	modified:   frontend/src/pages/Signup.tsx

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -4,7 +4,7 @@ import { Code2, ArrowLeft, Mail, Lock } from "lucide-react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import axios from "axios";
-import { useUser } from '../context/UserContext';
+import { useUser } from '../context/userContext'; // Correct the import path to match the actual file name
 import { BASE_URL , API_ENDPOINTS } from "../api";
 
 export const SignIn: React.FC = () => {
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -4,7 +4,7 @@ import { Code2, ArrowLeft, Mail, Lock, User } from "lucide-react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import axios from "axios";
-import { useUser } from '../context/UserContext';
+import { useUser } from '../context/userContext';
 import { BASE_URL , API_ENDPOINTS } from "../api";
 
 export const Signup: React.FC = () => {
